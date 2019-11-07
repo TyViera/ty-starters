5786e498af7ae2009ff869b576b8ba1f8f8130db
Nazaret Viera Ipanaque <nazaret.vieraipanaque@tcs.com>
2019-11-07 18:24:14 -0500

    feat: initial project configuration

---
+++ src/test/java/pe/ty/tystarters/TyStartersApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.ty.tystarters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TyStartersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
